44da4b8772dade9b9bb5d46be6890397dfb89d8c
Placaflaca00 <89082565+Placaflaca00@users.noreply.github.com>
2024-09-24 17:09:13 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,25 @@
+provider "google" {
+  credentials = file(var.credentials_file)
+  project     = var.project_id
+  region      = var.region
+  zone        = var.zone
+}
+
+resource "google_compute_instance" "default" {
+  name         = "mi-instancia"
+  machine_type = "e2-medium"
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+    }
+  }
+}
